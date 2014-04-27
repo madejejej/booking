d8c0a8bce8bbd9338cfc174ba164093cb051f502
Kacper Madej <kacper.madej@u2i.com>
2014-04-27 12:13:52 +0200

    Created backend for adding movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
   respond_to :json
 
+  before_action :require_organiser_authentication!, only: :create
+
   def index
     @movies = Movie.all
     respond_with @movies
@@ -11,4 +13,14 @@ class MoviesController < ApplicationController
     respond_with @movie
   end
 
+  def create
+    @movie = Movie.create(movie_params)
+    respond_with @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :cover_url, :duration, :director)
+  end
 end
