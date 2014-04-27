8b05e4f23710fe50fa475e047568f0e5a3caf6ea
Kacper Madej <kacper.madej@u2i.com>
2014-04-27 11:29:28 +0200

    Fixed tests for ticket type

--- spec/models/ticket_type_spec.rb
+++ spec/models/ticket_type_spec.rb
@@ -5,8 +5,8 @@ describe TicketType do
 
   it { should belong_to :show_type }
 
-  it { should validate_presence_of :type }
-  it { should validate_uniqueness_of :type }
+  it { should validate_presence_of :name }
+  it { should validate_uniqueness_of :name }
 
   it { should validate_presence_of :show_type }
 end
