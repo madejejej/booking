411662d775d3b7a4f4e0de17f8bb81e702dc67cf
root <akaterin@poczta.fm>
2014-05-22 16:18:23 +0200

    In order to run karma tests do the following: rake karma:install npm install karma-ng-scenario karma-coffee-preprocessor karma-jasmine karma-phantomjs-launcher --save-dev

---
+++ karma/lib/angular/controllers/CinemaController_spec.js.coffee
@@ -0,0 +1,7 @@
+describe "CinemaController", () ->
+
+
+
+  describe "validCinemaForm", () ->
+    it "should validate if all fields are filled", () ->
+      expect(false).toBeFalsy()
--- spec/controllers/cinemas_controller_spec.rb
+++ spec/controllers/cinemas_controller_spec.rb
@@ -31,9 +31,13 @@ describe CinemasController do
 
     context "for the logged in organiser" do
       let!(:user) { setup_organiser_sign_in }
-      let!(:cinemas) { FactoryGirl.create_list(:cinema, 3) }
+      let!(:cinemas) { FactoryGirl.create_list(:cinema, 3, organiser_data: user.organiser_data) }
       before do
         get :index, format: :json
+      end
+
+      it "retrieves all cinemas" do
+        assigns(:cinemas).should eq cinemas
       end
 
       it "ends with success" do
@@ -55,7 +59,6 @@ describe CinemasController do
     it "ends with success" do
       response.should be_success
     end
-
   end
 
   describe "#create" do
@@ -79,5 +82,4 @@ describe CinemasController do
   describe "#destroy" do
     include_examples "not authorized user", :delete, :destroy, id: 1
   end
-
 end
